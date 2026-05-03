7840577dd778a952d797168156efa584157da96a
dersim1949-beep <dersim1949@gmail.com>
2026-05-03 11:42:52 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Connecté en tant que {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("!ping"):
+        await message.channel.send("Pong!")
+
+client.run(os.getenv("DISCORD_TOKEN"))
